cbe3cedf780d8245007af00702e261790aa7f6bf
kalibri111 <livan.skin@yandex.ru>
2020-09-25 10:33:55 +0300

    common strcmp added

--- src/stringsort.c
+++ src/stringsort.c
@@ -5,57 +5,34 @@
 #include "../headers/stringsort.h"
 #include <ctype.h>
 
-
-int forward_strcmp(const void* str1, const void* str2) {
-    char* lhs_str = ((const strview_t*)str1)->str;
-    char* rhs_str = ((const strview_t*)str2)->str;
+int common_strcmp(const void* str1, const void* str2, size_t str1_begin, size_t str2_begin, int step) {
+    char* lhs_str = ((const strview_t*)str1)->str + str1_begin;
+    char* rhs_str = ((const strview_t*)str2)->str + str2_begin;
 
     while (*lhs_str && *rhs_str) {
 //        передвигаем указатель вперед, пока не найдем букву
         while (!isalpha(*lhs_str)) {
-            ++lhs_str;
+            lhs_str += step;
         }
         while (!isalpha(*rhs_str)) {
-            ++rhs_str;
+            rhs_str += step;
         }
 //        сравниваем
-        if (*lhs_str > *rhs_str) {
-            return 1;
-        } else if (*lhs_str < *rhs_str) {
-            return -1;
+        if (*lhs_str != *rhs_str) {
+            return *lhs_str - *rhs_str;
         }
-        ++lhs_str;
-        ++rhs_str;
+        lhs_str += step;
+        rhs_str += step;
     }
     return 0;
 }
 
-int reversed_strcmp(const void* str1, const void* str2) {
-//    идем с конца
-    const strview_t* str1_v = (const strview_t*)str1;
-    const strview_t* str2_v = (const strview_t*)str2;
+int forward_strcmp(const void* str1, const void* str2) {
+    return common_strcmp(str1, str2, 0, 0, 1);
+}
 
-    char* lhs_str = str1_v->str + str1_v->strlen - 1;
-    char* rhs_str = str2_v->str + str2_v->strlen - 1;
-    while (lhs_str - str1_v->str > 0 && rhs_str - str2_v->str > 0) {
-//        передвигаем указатель назад, пока не найдем букву
-        while (!isalpha(*lhs_str)) {
-            --lhs_str;
-        }
-        while (!isalpha(*rhs_str)) {
-            --rhs_str;
-        }
-//        сравниваем
-        if (*lhs_str > *rhs_str) {
-            return 1;
-        } else if (*lhs_str < *rhs_str) {
-            return -1;
-        }
-//        сдвигаем назад
-        --lhs_str;
-        --rhs_str;
-    }
-    return 0;
+int reversed_strcmp(const void* str1, const void* str2) {
+    return common_strcmp(str1, str2, ((const strview_t*)str1)->strlen, ((const strview_t*)str2)->strlen, -1);
 }
 
 /*
